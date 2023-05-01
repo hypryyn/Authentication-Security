7a674775d99d723f7c6d6604435b98f5754ce99a
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-05-01 20:33:11 +0700

    forget to add mongoose, and also add schema while at it

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB")
+    .then(() => console.log('Connected to MongoDB!'));
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.Schema("User", userSchema);
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.get("/register", function(req,res){
+    res.render("register");
+});
 
 
 
