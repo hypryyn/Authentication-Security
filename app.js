7b74350cfa0b1ffcb14d85c0362138244ab1d48c
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-05-01 20:57:57 +0700

    Lv1 of auth cred

--- app.js
+++ app.js
@@ -18,20 +18,49 @@ const userSchema = {
     password: String
 };
 
-const User = new mongoose.Schema("User", userSchema);
+const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     res.render("home");
 });
 
-app.get("/login", function(req,res){
+app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/register", function(req,res){
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+        .then(function () {
+            res.render("secrets")
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username })
+        .then(foundUser => {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
 
 
 app.listen(PORT, function () {
