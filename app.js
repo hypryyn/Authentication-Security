a414462c76b88cc487f77ee3eb8fba3adfe6f5a3
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-05-04 21:00:13 +0700

    fixing the updated passport logout

--- app.js
+++ app.js
@@ -50,10 +50,12 @@ app.get("/login", function (req, res) {
     res.render("login");
 });
 
-app.get("/logout", function(req, res){
-    req.logout();
-    res.redirect("/");
+app.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
   });
+});
 
 app.get("/register", function (req, res) {
     res.render("register");
